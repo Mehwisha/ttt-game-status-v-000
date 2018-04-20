2be0eacf6b0b91e9eab4e8825a5880270a103701
Mehwish Aziz <mehwisha@msn.com>
2018-04-20 00:05:28 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,35 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+   [0,1,2], #top_row_win
+   [3,4,5], #mid_row_win
+   [6,7,8], # bottom_row_win
+   [0,4,8], #diagonal_forward_win
+   [2,4,6], #diagonal_back_win
+   [0,3,6], #vertical_win_1
+   [1,4,7], #vertical_win_2
+   [2,5,8] #vertical_win_3
+]
+
+def won?(board)
+
+   WIN_COMBINATIONS.find do|win_combination|
+     board[win_combination[0]] == board[win_combination[1]] && board[win_combination[1]] == board[win_combination[2]] && position_taken?(board,win_combination[0])
+# since this will create new array with all (0,3,6,0,2,0,1,2) at 0 location and (1,4,7,4,4,3,4,5) at 1 location and (2,5,8,8,6,6,7,8) at 2 location
+   end
+ end
+
+def full?(board) #  Select, detect, reject,find
+      board.select do|win_combination|
+        win_combination.all?
+end
+end
+def draw?(board) #
+
+end
+def over?(board) #returns true if the board has been won, is a draw, or is full
+
+end
+def winner(board)
+end
